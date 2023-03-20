9e3d46b82b0655285909afdd939b387c1520cd2a
Vincent Naumann <vincentnaumann9@gmail.com>
2023-03-20 20:13:19 +0100

    added player and test map

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Player : MonoBehaviour
+{
+    public  float velocity = 5f;
+    public float rotationVelocity = 1000f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // WASD movement
+        float horizontalMovement = Input.GetAxisRaw("Horizontal");
+        float verticalMovement = Input.GetAxisRaw("Vertical");
+
+        Vector2 movementVector = new Vector2(horizontalMovement, verticalMovement);
+
+        transform.Translate(movementVector * velocity * Time.deltaTime, Space.World);
+
+        // Rotation
+        if (movementVector != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(movementVector.y, movementVector.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+}
